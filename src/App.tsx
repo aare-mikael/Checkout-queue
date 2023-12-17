75adb20093c8231eb7c8328e2a1453bb374a6d0b
Mikael Aare <aaremikael@gmail.com>
2023-12-17 04:13:43 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const generateRandomQueue = () => {
+    return Array.from({ length: 5 }, () =>
+      Array.from(
+        { length: Math.floor(Math.random() * 5) + 1 },
+        () => Math.floor(Math.random() * 10) + 1
+      )
+    );
+  };
+
+  const [itemsInCart, setItemsInCart] = useState<number>(0);
+  const [queues, setQueues] = useState(generateRandomQueue());
+  const [newItem, setnewItem] = useState<number | null>(null);
+
+  function addNewPersonToQueue(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (itemsInCart === undefined || itemsInCart <= 0) return;
+
+    let leastItemsAmount = 1e9; // 1e9 = 1 billion items
+    let shortestQueue: number[] | undefined = undefined;
+
+    for (let queue of queues) {
+      const itemsInQueue = queue.reduce((sum, value) => sum + value, 0);
+      if (itemsInQueue < leastItemsAmount) {
+        shortestQueue = queue;
+        leastItemsAmount = itemsInQueue;
+      }
+    }
+
+    if (!shortestQueue) return;
+    setQueues((prevQueues) =>
+      prevQueues.map((queue) =>
+        queue === shortestQueue ? [...queue, itemsInCart] : queue
+      )
+    );
+    setnewItem(itemsInCart);
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setQueues((prevQueues) =>
+        prevQueues.map((queue) => {
+          if (queue.length === 0) return queue;
+          const newQueue = [...queue];
+          const firstItem = newQueue[0];
+          if (firstItem > 1) {
+            newQueue[0] = firstItem - 1;
+          } else {
+            newQueue.shift();
+          }
+          setnewItem(null);
+          return newQueue;
+        })
+      );
+    }, 2000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <header className="header">
+          <h1>Checkout Queue Project</h1>
+          <p>
+            React practice project. This will simulate 5 checkout stations, with
+            5 corresponding queues. In your input box, enter the amount of items
+            you have in your shopping cart, and then you'll be put in the
+            shortest queue. Every 2 seconds, one item will be removed from each
+            queue. When you are at the front of the queue and "lose" your last
+            item, you will be removed from the queue.
+          </p>
+          <div className="form">
+            <form onSubmit={addNewPersonToQueue}>
+              <label htmlFor="items">Items:</label>
+              <input
+                required
+                type="number"
+                value={itemsInCart}
+                onChange={(e) => setItemsInCart(e.currentTarget.valueAsNumber)}
+              />
+              <button type="submit">Find shortest queue</button>
+            </form>
+          </div>
+        </header>
+        <body>
+          <div className="queues">
+            {queues.map((queue, idx) => (
+              <div className="queue" key={idx}>
+                <p>Queue {idx + 1}</p>
+                <div className="queue-items">
+                  {queue.map((numberOfItems, lineIdx) => (
+                    <div
+                      className={`queue-item ${
+                        numberOfItems === 1 && lineIdx === 0 ? "last-item" : ""
+                      } ${
+                        newItem === numberOfItems &&
+                        lineIdx === queue.length - 1
+                          ? "new-item"
+                          : ""
+                      }`}
+                      key={lineIdx}
+                    >
+                      {numberOfItems}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </body>
+      </main>
     </div>
   );
 }
